433c21d9a142d4e49f3ac7786f4ad68ff761d1d2
Eugene Mavrin <e.mavrin@skoltech.ru>
2021-09-15 12:57:03 +0300

    Add promo image and update page structure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import IconButton from '@material-ui/core/IconButton';
-import DoubleArrowOutlinedIcon from '@material-ui/icons/DoubleArrowOutlined';
 import { Avatar } from '@mui/material';
 
 const onDownload = () => {
@@ -21,42 +20,39 @@ export default function App() {
     <Container maxWidth="md">
       <Toolbar>
         <Avatar variant="rounded" alt="5Gst" src="/static/images/logo.png" />
-        <Typography variant="h5" fontWeight="fontWeightBold" style={{marginLeft: "10px"}}>
+        <Typography variant="h5" fontWeight="fontWeightBold" style={{marginLeft: "10px", flexGrow: 1}}>
           5Gst
         </Typography>
+        <IconButton href="https://github.com/SkoltechSummerCamp" target="_blank" rel="noopener noreferrer">
+          <GitHubIcon/>
+        </IconButton>
       </Toolbar>
-      <Box sx={{ my: 4 }}>
         <div align = "center" justify = "center" alignItems = "center">
-
-          <Typography variant="h4" component="h1" gutterBottom>
+          <Typography variant="h5" component="h1" gutterBottom>
             Opensource<br></br>
             iPerf-based<br></br>
-            5G speedtest service
+            <b>5G speedtest service</b>
           </Typography>
-          
-          <div className="carousel">
-          </div>
 
-          <div className="download_button"style={{marginTop: "100px", marginBottom: "100px"}}>
-            <Button variant="outlined" color="primary" size="large" onClick={onDownload}>
+          <Container maxWidth="xs">
+            <img
+              style={{ width: "100%"}}
+              alt="Cool bike"
+              src="/static/images/promo.png"
+            />
+          </Container>
+
+          <div className="download_button" style={{marginTop: "20px", marginBottom: "20px"}}>
+            <Button variant="contained" color="primary" size="large" onClick={onDownload}>
               Download Android App
             </Button>
             <Typography variant="caption" display="block" gutterBottom>
               0.0.1.66-develop
             </Typography>
           </div>
-
-          <div classname="more_info">
-            <Typography variant="h5" component="h1" gutterBottom>
-              More info
-            </Typography>
-            <IconButton href="https://github.com/SkoltechSummerCamp" target="_blank" rel="noopener noreferrer">
-              <GitHubIcon fontSize="large"/>
-            </IconButton>
-          </div>
-
         </div>
-      </Box>
+        <Box sx={{ my: 4 }}>
+</Box> 
     </Container>
   );
 }
